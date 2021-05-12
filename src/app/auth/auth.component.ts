748e0187ce78ea0e90343ee4879ec2b083515eb9
matteobavecchi <matteo.bavecchi@stud.unifi.it>
2021-05-12 10:44:00 +0200

    Added unique Observer for login and signup

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,7 +1,7 @@
 import { Component } from "@angular/core";
 import { NgForm } from "@angular/forms";
-import { onErrorResumeNext } from "rxjs";
-import { AuthService } from "./auth.service";
+import { Observable, onErrorResumeNext } from "rxjs";
+import { AuthService, AuthResponseData } from "./auth.service";
 
 @Component({
     selector: 'app-auth',
@@ -25,19 +25,24 @@ export class AuthComponent {
         }
         const email = form.value.email;
         const password = form.value.password;
+
+        let authObs: Observable<AuthResponseData>;
+
         this.isLoading = true;
         if (this.isLoginMode) {
-
+            authObs = this.authService.login(email, password);
         } else {
-            this.authService.signup(email, password).subscribe(responseData => {
-                console.log(responseData);
-                this.isLoading = false;
-            }, errorMessage => {
-                console.log(errorMessage);
-                this.error = errorMessage;
-                this.isLoading = false;
-            });
+            authObs = this.authService.signup(email, password);
         }
+
+        authObs.subscribe(responseData => {
+            console.log(responseData);
+            this.isLoading = false;
+        }, errorMessage => {
+            console.log(errorMessage);
+            this.error = errorMessage;
+            this.isLoading = false;
+        });
 
         form.reset();
     }
