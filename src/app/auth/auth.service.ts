bbe74c305fa7d4198cbb9d815c3836587d06b06d
matteobavecchi <matteo.bavecchi@stud.unifi.it>
2021-05-13 14:04:35 +0200

    Added API key to environment

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -4,8 +4,7 @@ import { catchError, tap } from "rxjs/operators";
 import { BehaviorSubject, throwError } from "rxjs";
 import { User } from "./user.model";
 import { Router } from "@angular/router";
-
-
+import { environment } from '../../environments/environment';
 export interface AuthResponseData {
     kind: string,
     idToken: string,
@@ -25,7 +24,7 @@ export class AuthService {
     constructor(private http: HttpClient, private router: Router) { }
 
     signup(email: string, password: string) {
-        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC-aVBfakfEVaSo7EcYbTb9wOKPXDTeOBA',
+        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + environment.firebaseAPIKey,
             {
                 email: email,
                 password: password,
@@ -43,7 +42,7 @@ export class AuthService {
     }
 
     login(email: string, password: string) {
-        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyC-aVBfakfEVaSo7EcYbTb9wOKPXDTeOBA',
+        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + environment.firebaseAPIKey,
             {
                 email: email,
                 password: password,
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseAPIKey: 'AIzaSyC-aVBfakfEVaSo7EcYbTb9wOKPXDTeOBA'
 };
